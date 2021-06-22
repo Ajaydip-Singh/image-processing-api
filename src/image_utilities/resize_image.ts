e54ebfc87813e58d8648974e08ccc0b2be2905e2
Ajaydip Singh <singh@minerva.kgi.edu>
2021-06-22 11:50:35 +0300

    :sparkles: feat: add image resizing function

--- src/image_utilities/resize_image.ts
+++ src/image_utilities/resize_image.ts
@@ -1,3 +1,26 @@
-import sharp from "sharp";
+import sharp, { FitEnum } from "sharp";
 
-sharp('')
+/* 
+Resize image based on input parameters. 
+By default the image is resized based on 
+the cover option and position center. 
+*/
+const resizeImage = (
+  inputImagePath: string,
+  outputImagePath: string,
+  width: number,
+  height: number,
+  resizeOption = "cover",
+  position = "centre"
+): void => {
+  sharp(inputImagePath)
+    .resize(width, height, {
+      fit: resizeOption as keyof FitEnum,
+      position: position,
+    })
+    .toFile(outputImagePath, (err) => {
+      console.log(err);
+    });
+};
+
+export default resizeImage;
