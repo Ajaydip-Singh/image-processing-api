d24534d3c414ed0d2d2d83896d4523327097b5ad
Ajaydip Singh <singh@minerva.kgi.edu>
2021-07-01 14:28:48 +0300

    :sparkles: feat: serve readme file in base url

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var index_1 = __importDefault(require("./routes/index"));
+var path_1 = __importDefault(require("path"));
 // Initialize server
 var app = express_1.default();
 // Set a port
 var port = 3000;
 app.use("/api", index_1.default);
+app.get("/", function (req, res) {
+    var readmePath = path_1.default.resolve(__dirname, "../README.md");
+    res.sendFile(readmePath);
+});
 app.listen(port, function () {
     console.log("server started at localhost:" + port);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import routes from "./routes/index";
+import path from "path";
 
 // Initialize server
 const app = express();
@@ -7,6 +8,11 @@ const app = express();
 const port = 3000;
 
 app.use("/api", routes);
+
+app.get("/", (req: express.Request, res: express.Response): void => {
+  const readmePath = path.resolve(__dirname, "../README.md");
+  res.sendFile(readmePath);
+});
 
 app.listen(port, () => {
   console.log(`server started at localhost:${port}`);
